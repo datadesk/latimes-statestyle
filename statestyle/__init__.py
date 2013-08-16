8632bd101f766dec493521ccbd3927f5e899acba
palewire <ben.welsh@gmail.com>
2013-08-16 07:40:51 -0700

    PEP8 compliance

--- statestyle/__init__.py
+++ statestyle/__init__.py
@@ -5,9 +5,9 @@ from .data import CROSSWALK
 def get(value):
     """
     Accepts a value and tries to match it a U.S. state or territory.
-    
-    Works with postal abbreviations, full names, AP abbreviations and FIPS codes.
-    
+
+    Works with postal codes, full names, AP abbreviations and FIPS codes.
+
     Return an object with metadata about the state.
     """
     # Strip leading zeroes of any fips that come in as strings
@@ -38,13 +38,13 @@ class State(object):
         self.ap = ap
         self.type = type
         self.stateface = stateface
-    
+
     def __repr__(self):
         return '<%s: %s>' % (self.__class__.__name__, self.name)
-    
+
     def __str__(self):
         return self.__unicode__().encode("utf-8")
-    
+
     def __unicode__(self):
         return self.name
 
